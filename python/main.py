757e45dc2fb743b5cc0f4543ab3d551ef3cc1898
Elijah Mickelson <elijah.mickelson@ucalgary.ca>
2025-07-10 16:09:28 -0600

    Test code

--- python/main.py
+++ python/main.py
@@ -1,3 +1,18 @@
 import torch
+import numpy as np
+import nrrd
 
-print("PyTorch version:", torch.__version__)
+dir = "python/dump/"
+
+data = np.zeros((1, 2, 3, 4))
+filename = "lgemri.nrrd"
+filepath = dir + filename
+
+# write
+# nrrd.write(filepath, data)
+
+readdata, header = nrrd.read(filepath)
+print(readdata.shape)
+print(header)
+
+print(set(readdata.flatten()))
