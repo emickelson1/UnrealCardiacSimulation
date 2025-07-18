dd8a7d9649d1dbe8e54f3b8c4d65d155f9aadee3
Elijah Mickelson <elijah.mickelson@ucalgary.ca>
2025-07-18 13:36:15 -0600

    removed bioxel heart model versions

--- python/main.py
+++ python/main.py
@@ -6,20 +6,23 @@ from pathlib import Path
 
 unprocessed_data_dir = f"{Path("./main.py").parent.absolute()}/data/unprocessed"
 dataset_name = input("Enter the dataset name: ")
+dataset_name = dataset_name if len(dataset_name) > 0 else "default"
 seg_paths = glob.glob(f"{unprocessed_data_dir}/{dataset_name}/*_seg.nrrd")
 vol_paths = [path.replace("_seg.nrrd", "_vol.nrrd") for path in seg_paths]
 
 for i in range(0, len(seg_paths)):
     print(f"seg: {seg_paths[i]}\nvol: {vol_paths[i]}\n")
 
-seg_data = [nrrd.read_data(path) for path in seg_paths]
-vol_data = [nrrd.read_data(path) for path in vol_paths]
+seg_data = [nrrd.read(path)[0] for path in seg_paths]
+vol_data = [nrrd.read(path)[0] for path in vol_paths]
 
-for data in seg_data:
-    print(data.shape)
+# for data in seg_data:
+#     print(data.shape)
 
-# readdata, header = nrrd.read(filepath)
+# readdata, header = nrrd.read(seg_paths[0])
+# print(set(readdata.flatten()))
+
 # print(readdata.shape)
 # print(header)
 
-# print(set(readdata.flatten()))
+print(set(seg_data[0].flatten()))
