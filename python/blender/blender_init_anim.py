f0620803222f8a429442460babf2f87d4a7070ec
Elijah Mickelson <elijah.mickelson@ucalgary.ca>
2025-07-29 14:35:01 -0600

    Finished automatic animation setup script

--- python/blender/blender_init_anim.py
+++ python/blender/blender_init_anim.py
@@ -25,21 +25,32 @@ ORGAN_PAIRS = {
     "a":    -1                      # aorta                 use -1 as temp value
 }
 
-# Store the result of the last data query
+# Store the results of the last data query
 loaded_data = None
+frame_rate = -1
+frame_count = -1
+
+# Constant object names
+custom_prop_name = "HeartControl"
 
 
 def main():
     # Load data
     load_data()
 
-    # Run the update script for frame 0
-    update_organ_volumes("frame", 0)
+    # Initialize custom properties
+    get_custom_properties()
+
+    # Insert keyframes for each frame
+    insert_keyframes("frame", list(range(int(frame_count))))
+
+    # Return to object mode
+    bpy.ops.object.mode_set(mode='OBJECT')
 
 
 def load_data() -> bool:
     """Load the spreadsheet data in the assets folder. Returns true if the data exists, false otherwise."""
-    global loaded_data
+    global loaded_data, frame_count, frame_rate
 
     # Get blend file path
     blend_path = bpy.data.filepath
@@ -50,7 +61,7 @@ def load_data() -> bool:
         proj_dir = os.path.dirname(proj_dir)
 
     # Append relative path of anim_data.csv to get data path
-    data_path = os.join(proj_dir, "/assets/anim_data.csv")
+    data_path = os.path.join(proj_dir, "assets/anim_data.csv")
 
     # Verify data path exists
     if not os.path.exists(data_path):
@@ -65,66 +76,166 @@ def load_data() -> bool:
         read_row = False
         anim_data = []      # 2D array. First index is for row, second is for column.
         for row in reader:
-            if not row or row[0] is "":
+            # Skip empty rows
+            if not row or row[0] == "":
                 read_row = False
                 continue
+
+            # Read frame_rate, frame_count
+            if row[0] == "frame rate":
+                frame_rate = int(row[1])
+                continue
+
+            if row[0] == "frame count":
+                frame_count = int(row[1])
+                continue
+
+            # After reading "Phase", the next lines are data until an empty line is reached
             if "Phase" in row[0]:
                 read_row = True
                 continue
+
             if read_row:
                 anim_data.append(row)
+                continue
 
     # Set global variable to the loaded data
     loaded_data = anim_data
     return True
 
 
-def update_organ_volumes(req_metric: str, req_value):
-    """Get the volume for all organs at a specific phase, time, or frame."""
+def insert_keyframes(req_metric: str, req_values: list) -> bool:
+    """Given a metric ('frame' or 'phase'), and a list of discrete values, insert keyframes for the animation of each organ."""
     global loaded_data
+
+    # Ensure data is loaded
+    if loaded_data is None:
+        print("Error: Data failed to load.")
+        return False
 
     # Update volumes based on phase input
     if req_metric.lower().strip() == "phase":
-        phase = int(req_value)
-        phase_row_index = phase - 1   # since row 0 corresponds to phase 1, row 1 to phase 2, etc.
-        for organ in ORGAN_PAIRS:
-            if organ.key is not -1:
-                organ_name = organ.key
-                organ_column_index = organ.value
-                _update_organ_volume(organ_name, loaded_data[phase_row_index][organ_column_index])
+        for phase in req_values:
+            phase_row_index = int(phase) - 1   # since row 0 corresponds to phase 1, row 1 to phase 2, etc.
+            for organ_name in ORGAN_PAIRS:
+                if organ_name.key != -1:
+                    organ_name = organ_name.key
+                    phase_frame = [int(data) for data in loaded_data[phase_row_index][START_FRAME_INDEX]] # get the starting frame corresponding to this phase
+                    organ_column_index = organ_name.value
+                    _insert_keyframe(organ_name, phase_frame, [float(data) for data in loaded_data[phase_row_index][organ_column_index]])
         
         return True
 
     # Update volumes based on frame input
     elif req_metric.lower().strip() == "frame":
-        frame = float(req_value)
-        
         # Always interpolate frames to ensure continuity:
-        for organ in ORGAN_PAIRS:
-            if organ.key is not -1:
-                organ_name = organ.key
-                organ_column_index = organ.value
+        for organ_name in ORGAN_PAIRS:
+            # Get organ column index
+            organ_column_index = ORGAN_PAIRS.get(organ_name)
+            if organ_column_index == -1:
+                print(f"Warning: No data available for organ '{organ_name}'. Skipping.")
+                continue
 
-                # Using numpy regression, approximate a polynomial for the volume of the organ at each frame
-                x = np.array(loaded_data[:, START_FRAME_INDEX])
-                y = np.array(loaded_data[:, organ_column_index])
-                polynomial = np.poly1d(np.polyfit(x, y, 3))
+            # Using numpy regression, approximate a polynomial for the volume of the organ at each frame
+            x = np.array([float(loaded_data[row][START_FRAME_INDEX]) for row in range(len(loaded_data))])
+            y = np.array([float(loaded_data[row][organ_column_index]) for row in range(len(loaded_data))])
+            polynomial = np.poly1d(np.polyfit(x, y, 3))
 
-                # Update the organ volume with the approximated volume at the input frame 
+            # Update the organ volume with the approximated volume at the input frame
+            for frame in req_values:
+                frame = int(frame)
                 value = polynomial(frame)
-                _update_organ_volume(organ_name, value)
+                _insert_keyframe(organ_name, frame, value)
         
         return True
 
-    # Handle errors
+    # Handle bad input
     else:
         print(f"Error: Could not recognize the input '{req_metric}'. Try 'phase' or 'frame'.")
         return False
 
 
-def _update_organ_volume(organ: str, value: float):
-    """Update the volume of the organ given by the argument."""
-    # Find the organ object 
+def get_custom_properties() -> bpy.types.Object:
+    """If the custom properties object is missing, create it."""
+    global custom_prop_name
+
+    # Try to find the custom properties object
+    obj = bpy.data.objects.get(custom_prop_name)
+
+    # If the empty properties object does not exist, initialize it
+    if not obj:
+        bpy.ops.object.empty_add(type='PLAIN_AXES', align='WORLD', location=(0, 0, 0))
+        obj = bpy.context.object
+        obj.name = custom_prop_name
+
+        # Initialize custom properties
+        for organ_name in ORGAN_PAIRS:
+            obj[organ_name] = -1
+    
+    return obj
+
+
+def _insert_keyframe(organ_name: str, frame: int, value: float) -> bool:
+    """Insert a keyframe for the organ. This controls the volume of the organ over time."""
+
+    # Add the keyframe to the custom properties object
+    custom_prop_obj = get_custom_properties()
+    custom_prop_obj[organ_name] = value
+    custom_prop_obj.keyframe_insert(data_path=f'["{organ_name}"]', frame=frame)
+
+    # Find the organ object
+    obj = bpy.data.objects[organ_name]
+    if not obj:
+        print(f"Could not find object by name '{organ_name}' in scene.")
+        return False
+    
+    # Find the bone and ensure that the driver is configured
+    for modifier in obj.modifiers:
+        if modifier.type == 'ARMATURE':
+            armature_obj = modifier.object
+            armature_data = armature_obj.data
+
+            # Focus on armature, swap to pose mode
+            bpy.context.view_layer.objects.active = armature_obj
+            bpy.ops.object.mode_set(mode='POSE')
+
+            # Get bone
+            bone = list(armature_data.bones)[0].name        # we are assuming that each organ has only one bone
+            pose_bone = armature_obj.pose.bones[bone]
+
+            # Get data path for scale vector
+            data_path = pose_bone.path_from_id("scale")
+
+            # Ensure the armature obj has animation data
+            if not armature_obj.animation_data:
+                armature_obj.animation_data_create()
+
+            # Initialize driver if it does not exist
+            _init_driver(pose_bone, data_path, organ_name)
+
+
+def _init_driver(pose_bone: bpy.types.PoseBone, data_path: str, organ_name: str):
+    # Expression in v1 was "x*u*(pow(y, 1/3))/5 + (1-u)". No idea why I cube rooted the volume.
+    # Init driver
+    for i in range(3):
+        d = pose_bone.id_data.driver_add(data_path, i)      # i is the index for x, y, z scale
+        
+        # Make first variable (Volume)
+        var1 = d.driver.variables.new()
+        var1.name = "volume"
+        var1.targets[0].id = bpy.data.objects[custom_prop_name]
+        var1.targets[0].data_path = f'["{organ_name}"]'
+
+        # Get mean volume
+        mean = _get_mean_volume(ORGAN_PAIRS.get(organ_name))
+
+        # Set driver expression
+        d.driver.expression = f"{var1.name}/{mean}"
+
+
+def _get_mean_volume(organ_index: int) -> float:
+    values = [float(row[organ_index]) for row in loaded_data]
+    return sum(values) / float(len(values))
 
 
 if __name__ == "__main__":
